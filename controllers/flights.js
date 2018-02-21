a1261879b956724bb3aa43189024ce49b252db87
Chigozie Ekwonu <chygoz2@gmail.com>
2018-02-21 19:48:57 +0100

    Booking information now saved in database

--- controllers/flights.js
+++ controllers/flights.js
@@ -2,8 +2,10 @@ require('dotenv').load();
 const router = require('express').Router();
 const axios = require('axios');
 const sha1 = require('sha1');
+const moment = require('moment');
 const Country = require('../models/Country');
 const Token = require('../models/Token');
+const Booking = require('../models/Booking');
 const { FLIGHT_TRIP_TYPE, TICKET_CLASS_TYPE, AGE_GROUPS } = require('../config/constants');
 
 const URL = process.env.URL;
@@ -107,10 +109,24 @@ router.post('/create-booking', async (req,res) => {
         .then(response => {
             if(response.data.status === 0){
                 //write code to save booking details to database
-                const { referenceNumber, bookingNumber, ticketLimitDate } = response.data.data;
+                const { referenceNumber, bookingNumber } = response.data.data;
                 //insert into database code
-
-                res.status(200).json(response.data);
+                const booking = new Booking({
+                    user: {},
+                    referenceNumber: referenceNumber,
+                    bookingNumber: bookingNumber,
+                    bookingDate: moment().format("DD/MM/YY gg:mm"),
+                    reservationStatus: "UNPROCESSED",
+                    pricedItinerary: input.pricedItinerary,
+                    contactInformation: input.contactInformation,
+                    travellers: input.travellers  
+                });
+                booking.save((err) => {
+                    if(err){
+                        return res.status(500).json({status: -1, message: "Booking submitted. However an error occured while saving booking details to database"});
+                    }
+                    res.status(200).json(response.data);
+                });
             }else{
                 return res.status(500).json({status: results.status, message: results.message});
             }
@@ -133,9 +149,16 @@ router.post('/cancel-booking', async (req, res) => {
                 const { successful } = response.data.data;
                 if(successful){
                     //update database code
+                    Booking.findOneAndUpdate({bookingNumber: input.bookingNumber}, {reservationStatus: "CANCELLED"}, (err, data) => {
+                        if(err){
+                            return res.status(500).json({status: -1, message: "Booking cancelled. However an error occured while update booking details in database"});
+                        }
+                        if(!data){
+                            return res.status(500).json({status: -1, message: "Booking cancelled. However was unable to find booking record in database"});
+                        }
+                        res.status(200).json(response.data);
+                    });
                 }
-
-                res.status(200).json(response.data);
             }else{
                 return res.status(500).json({status: results.status, message: results.message});
             }
---
+++ models/Booking.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookingSchema = new Schema({
+    user: { 
+        firstname: {type: String },
+        lastname: {type: String },
+        email: { type: String }
+    },
+    referenceNumber: { type: String },
+    bookingNumber: { type: String },
+    bookingDate: { type: String },
+    reservationStatus: {type: String},
+    pricedItinerary: {
+        sequencyNumber: Number,
+        totalFare: String,
+        currencyCode: String,
+        ticketLimitTime: String,
+        signature: String,
+        originDestinationOptions: [
+            {
+                stops: Number,
+                flightSegments: [
+                    {
+                        departureTime: String,
+                        arrivalTime: String,
+                        departureAirportCode: String,
+                        departureAirportName: String,
+                        arrivalAirportCode: String,
+                        arrivalAirportName: String,
+                        operatingAirlineCode: String,
+                        airlineName: String,
+                        airlineCode: String,
+                        bookingClass: String,
+                        journeyDuration: String,
+                        flightNumber: String,
+                        resBookDesigCode: String,
+                        numberInParty: Number,
+                        rph: Number
+                    }
+                ]
+            }
+        ],
+    },
+    contactInformation: {
+        title: Number,
+        firstName: String,
+        lastName: String,
+        dateOfBirth: String,
+        email: String,
+        phoneNumber: String,
+        address: String,
+        city: String,
+        countryCode: String
+    },
+    travellers: [
+    	{
+    	  firstName: String,
+	      lastName: String,
+	      dateOfBirth: String,
+	      title: Number,
+	      ageGroup: Number,
+	      passportNumber: String,
+	      passportIssuingCountryCode: String,
+	      passportExpiryDate: String
+    	}
+    ]
+});
+
+module.exports = mongoose.model('Booking', bookingSchema);
